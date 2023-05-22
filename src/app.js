fec4b2132efdb3ce899f383864be5a857fc17e14
artndev <97910780+artndev@users.noreply.github.com>
2023-05-22 19:56:45 +0300

    Remarks

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 import express from "express";
 import { saveQuoteValidation, idParamValidation } from "./validations.js";
-import { deleteQuote, findQuote, saveQuote } from "./controllers/Controller.js";
+import { deleteQuote, getQuote, getRandomQuote, saveQuote } from "./controllers/Controller.js";
 import handleValidationErrors from "./handleValidationErrors.js";
 const app = express();
 app.use(express.json());
 
 
 app.get("/", (_, res) => {
-    res.status(200).json({ message: "The QUOTES-REST-API" })
+    res.status(200).json({
+        message: "The QUOTES-REST-API. It's very simple but useful! :)",
+        description: {
+            createANewQuote: "[POST] /quotes",
+            deleteAQuote: "[DELETE] /quotes/:id",
+            getAQuote: "[GET] /quotes/:id",
+            getARandomQuote: "[GET] /random",
+        }
+    });
 });
+app.get("/quotes/:id", idParamValidation, handleValidationErrors, getQuote);
 app.post("/quotes", saveQuoteValidation, handleValidationErrors, saveQuote);
-app.get("/quotes/:id", idParamValidation, handleValidationErrors, findQuote);
-app.delete("/quotes/:id", idParamValidation, handleValidationErrors, deleteQuote)
+app.delete("/quotes/:id", idParamValidation, handleValidationErrors, deleteQuote);
+app.get("/random", getRandomQuote);
 
 
 app.listen(8000, (err) => {
@@ -20,5 +29,5 @@ app.listen(8000, (err) => {
         return;
     }
 
-    console.log("The server is listening on port 8000!");
+    console.log("The server is listening on the port / Сервер на порту - 8000!\nhttps://localhost:8000/");
 });
--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -8,154 +8,140 @@ export const saveQuote = async (req, res) => {
     try {
         await mongoose.connect(process.env.CONNECTION_URI);
 
-        return await (new Quote({
+        const doc = await new Quote({
             author: req.body.author,
             text: req.body.text
         }).save()
-            .then((doc) => {
-                if (doc) {
-                    res.status(200).json({
-                        message: "Цитата успешно сохранена!",
-                        result: {
-                            code: res.statusCode,
-                            quote: doc
-                        }
-                    });
-                    return doc;
+        if (doc) {
+            res.status(200).json({
+                message: "Цитата успешно сохранена!",
+                result: {
+                    code: res.statusCode,
                 }
-                else
-                    throw new Error;
-            })
-            .catch((err) => {
-                if (err instanceof Error) {
-                    console.error(err);
-
-                    res.status(400).json({
-                        message: "Не удалось сохранить цитату.",
-                        error: {
-                            name: err.name,
-                            code: res.statusCode
-                        }
-                    });
-                }
-                return null;
-            }));
+            });
+            return;
+        }
+        else
+            throw new Error;
     } 
     catch (err) {
         if (err instanceof Error) {
             console.error(err);
 
             res.status(500).json({
-                message: "Сервер не отвечает...",
+                message: "Не удалось сохранить цитату.",
                 error: {
+                    code: res.statusCode,
                     name: err.name,
-                    code: res.statusCode
                 }
             });
         }
-        return null;
+        return;
     }
 }
 
 
-export const findQuote = async (req, res) => {
+export const getQuote = async (req, res) => {
     try {
         await mongoose.connect(process.env.CONNECTION_URI);
 
-        return await (Quote.findById(req.params.id)
-            .then((doc) => {
-                if (doc) {
-                    res.status(200).json({
-                        message: "Цитата найдена!",
-                        result: {
-                            code: res.statusCode,
-                            quote: doc
-                        }
-                    });
-                    return doc;
+        const doc = await Quote.findById(req.params.id)
+        if (doc) {
+            res.status(200).json({
+                message: "Цитата найдена!",
+                result: {
+                    code: res.statusCode,
+                    _doc: doc,
                 }
-                else
-                    throw new MongooseError;
-            })
-            .catch((err) => {
-                if (err instanceof Error) {
-                    console.error(err);
-        
-                    res.status(404).json({
-                        message: "Не удалось найти цитату.",
-                        error: {
-                            name: err.name,
-                            code: res.statusCode
-                        }
-                    });
-                }
-                return null;
-            }));
+            });
+            return;
+        }
+        else
+            throw new MongooseError;
     }
     catch (err) {
         if (err instanceof Error) {
             console.error(err);
 
-            res.status(500).json({
-                message: "Сервер не отвечает...",
+            res.status(404).json({
+                message: "Не удалось найти цитату.",
                 error: {
+                    code: res.statusCode,
                     name: err.name,
-                    code: res.statusCode
                 }
             });
         }
-        return null;
+        return;
     }
 }
 
 
 export const deleteQuote = async (req, res) => {
     try {
-
         await mongoose.connect(process.env.CONNECTION_URI);
 
-        return await (Quote.deleteOne({ _id: req.params.id })
-            .then((doc) => {
-                if (doc.deletedCount > 0) {
-                    res.status(200).json({
-                        message: "Цитата успешно удалена!",
-                        result: {
-                            code: res.statusCode,
-                            quote: doc
-                        }
-                    });
-                    return doc;
+        const doc = await Quote.deleteOne({ _id: req.params.id });
+        if (doc.deletedCount > 0) {
+            res.status(200).json({
+                message: "Цитата успешно удалена!",
+                result: {
+                    code: res.statusCode,
                 }
-                else
-                    throw new MongooseError;
-            })
-            .catch((err) => {
-                if (err instanceof Error) {
-                    console.error(err);
-        
-                    res.status(404).json({
-                        message: "Не получилось удалить цитату.",
-                        error: {
-                            name: err.name,
-                            code: res.statusCode
-                        }
-                    });
-                }
-                return null;
-            }));
+            });
+            return;
+        }
+        else
+            throw new MongooseError;
     }
     catch (err) {
         if (err instanceof Error) {
             console.error(err);
 
-            res.status(500).json({
-                message: "Сервер не отвечает...",
+            res.status(404).json({
+                message: "Не получилось удалить цитату.",
                 error: {
+                    code: res.statusCode,
                     name: err.name,
-                    code: res.statusCode
                 }
             });
         }
-        return null;
+        return;
+    }
+}
+
+
+export const getRandomQuote = async (_, res) => {
+    try {
+        await mongoose.connect(process.env.CONNECTION_URI);
+
+        const arr = await Quote.find()
+        if (arr.length > 0) {
+            const doc = arr[Math.floor(Math.random() * arr.length)]
+
+            res.status(200).json({
+                message: "Случайная цитата получена!",
+                result: {
+                    code: res.statusCode,
+                    _doc: doc,
+                }
+            });
+            return;
+        }
+        else
+            throw new MongooseError;
+    }
+    catch (err) {
+        if (err instanceof Error) {
+            console.error(err);
+
+            res.status(404).json({
+                message: "Не удалось получить случайную цитату.",
+                error: {
+                    code: res.statusCode,
+                    name: err.name,
+                }
+            });
+        }
+        return;
     }
 }
